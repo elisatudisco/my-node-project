ae2ec9780fbbcba8f97fff28a5243ded9544704e
elisatudisco <elisa.tudisco@icloud.com>
2022-11-23 12:30:39 +0100

    websocket

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,17 @@
+//load the library
+let clientSocket = io();
+
+clientSocket.on("connect", newConnection);
+
+function newConnection() {
+  console.log(clientSocket.io);
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 20)
+  circle(mouseX, mouseY, 20);
 }
